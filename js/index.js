e39f10cf1150b329a44810bed300c2348be19767
Ignacio Labra <ignaciolsaavedra@gmail.com>
2023-09-29 17:14:04 -0300

    Subida de datos

--- js/index.js
+++ js/index.js
@@ -1,95 +1,213 @@
-alert('Hoy 10% de descuento si superas los $1000 pesos en el total')
-//variables que recogen id's desde el HTML
-const registrarProducto = document.querySelector('#agregarProductos')
-const campoNombre = document.querySelector('#campoNombre')
-const campoCategoria = document.querySelector('#campoCategoria')
-const campoPrecio = document.querySelector('#campoPrecio')
-const campoCantidad = document.querySelector('#campoCantidad')
+const divMostrar = document.querySelector('#mostradorProductos');
+const formFiltrar = document.querySelector('#filtroForm')
 const tablaParaMostrar = document.querySelector('#tabla')
+const mostrarCarritoBtn = document.getElementById('mostrarCarritoBtn');
 const mostarTablas = document.querySelector('#escondido')
 const infoDelTotal = document.querySelector('#totalInfo')
-const carrito = JSON.parse(localStorage.getItem('carrito')) || []
-      mostrarProductos()
-//Construcción del objeto
-class Producto {
-    constructor({nombre, categoria, precio, cantidad, valor}) {
-      this.nombre = nombre
-      this.categoria = categoria
-      this.precio = precio
-      this.cantidad = cantidad
-      this.valor = valor
-    }
-  }
-//función para agregar los objetos
-    registrarProducto.onsubmit = e => {
-    e.preventDefault()
-    const nombre = campoNombre.value
-    const categoria = campoCategoria.value
-    const precio = campoPrecio.value
-    const cantidad = campoCantidad.value
-    const valor = precio * cantidad 
-    const producto = new Producto({nombre,categoria, precio, cantidad, valor })
-    guardarEnCarrito(producto)
-  }
-  //función para guardar los objetos en el array
-  function guardarEnCarrito(producto) {
-    carrito.push(producto)
-    localStorage.setItem('carrito', JSON.stringify(carrito))
+const carrito = JSON.parse(localStorage.getItem('carrito')) || [];
+
+// Evento para mostrar el carrito apretando el botón del nav
+mostrarCarritoBtn.addEventListener('click', () => {
+  if (carrito.length > 0) {
     mostrarProductos()
+  } else {
+    Swal.fire({
+      title: 'Carrito Vacío',
+      text: 'Intenta agregar algunos productos :)',
+      backdrop: 'rgba(0, 0, 0, 0.8)',
+      imageUrl: 'https://i.gifer.com/7VE.gif',
+      imageWidth: 350,
+      imageHeight: 350,
+      imageAlt: 'jhon travolta meme',
+    })
   }
-   //función para mostrar los productos en una tabla html
-  function mostrarProductos(){
-   if (carrito.length > 0) {
-    mostarTablas.style.display="block"
-    let tablaBucle= ''
-      for(let i = 0; i < carrito.length; i++){  
-         tablaBucle += `<tr>
-                  <td>${carrito[i].nombre}</td>
-                  <td>${carrito[i].categoria}</td>
-                  <td>${carrito[i].precio}</td>
-                  <td>${carrito[i].cantidad}</td>
-                  <td>${carrito[i].valor}</td>
-                  <td align="center"><a class="eliminar" onclick="eliminarProductos(${i})"> Eliminar </a></td>
-                  </tr> `
-      }   
-        tablaParaMostrar.innerHTML = tablaBucle
-      sumadoraProductos()
-    }
+});
+
+//función para mostrar la consulta al json con los productos y mostrarlos en pantalla
+function mostrarDiv() {
+  fetch('/js/productos.json')
+    .then(res => res.json())
+    .then(obj => {
+      const productos = obj
+      let productoDiv = ''
+
+      for (let i = 0; i < productos.length; i++) { 
+        productoDiv += `<div class="col-md-3">
+                            <div class="card">
+                            <img src="img/${productos[i].codigo}.avif" class="card-img-top" alt="${productos[i].descripcion}">
+                            <div class="card-body">
+                              <span class="card-text categoriaP">${productos[i].categoria}</span>
+                              <h5 class="card-title nombreP">${productos[i].nombre}</h5>
+                              <p class="card-text descripcionP">${productos[i].descripcion}.</p>
+                              <h5 class="precioP">$${productos[i].precio}</h5>
+                              <div class="card-footer d-flex justify-content-center">
+                              <a class="btn btn-primary agregar-producto" data-codigo="${productos[i].codigo}">Agregar</a>
+                              </div>
+                           </div>
+                         </div>
+                       </div>
+                       `;
+      }
+      divMostrar.innerHTML = productoDiv;
+      const botonesAgregar = document.querySelectorAll('.agregar-producto');
+      botonesAgregar.forEach(boton => {
+        boton.addEventListener('click', (event) => agregarAlCarrito(event, productos)); 
+      });
+    formFiltrar.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const categoriaSeleccionada = formFiltrar.elements.categoria.value;
+    const productosFiltrados = categoriaSeleccionada === 'todos' ? productos : productos.filter(producto => producto.categoria === categoriaSeleccionada)
+    mostrarProductosFiltrados(productosFiltrados);
+  });
+    });
 }
-//función para sumar el valor total de los productos y crear una tabla
-  function sumadoraProductos(){ 
-  let total = 0 
-  const pre = carrito.map( preXcan => preXcan.valor)
+
+//función para filtrar los productos por su categoría respondiendo al evento del form
+function mostrarProductosFiltrados(productos) {
+    divMostrar.innerHTML = '';
+    productos.forEach(producto => {
+      const productoHTML = `
+        <div class="col-md-3">
+          <div class="card">
+            <img src="img/${producto.codigo}.avif" class="card-img-top" alt="${producto.descripcion}">
+            <div class="card-body">
+              <span class="card-text categoriaP">${producto.categoria}</span>
+              <h5 class="card-title nombreP">${producto.nombre}</h5>
+              <p class="card-text descripcionP">${producto.descripcion}.</p>
+              <h5 class="precioP">$${producto.precio}</h5>
+              <div class="card-footer d-flex justify-content-center">
+              <a class="btn btn-primary agregar-producto" data-codigo="${producto.codigo}">Agregar</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      `;
+      divMostrar.innerHTML += productoHTML;
+    });
+    const botonesAgregar = document.querySelectorAll('.agregar-producto');
+    botonesAgregar.forEach(boton => {
+      boton.addEventListener('click', (event) => agregarAlCarrito(event, productos));
+    });
+  }
+// función que agrega el producto al carrito  a partir del evento y del producto seleccionado
+function agregarAlCarrito(event, productos) {
+    const codigoProducto = event.target.getAttribute('data-codigo');
+    const productoEncontrado = productos.find(producto => producto.codigo === codigoProducto);
+    if (productoEncontrado) {
+      const productoEnCarrito = carrito.find(item => item.codigo === codigoProducto);
+      if (productoEnCarrito) {
+        productoEnCarrito.cantidad += 1;
+      } else {
+        productoEncontrado.cantidad = 1;
+        carrito.push(productoEncontrado);
+      }
+      localStorage.setItem('carrito', JSON.stringify(carrito));
+      Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        title: 'Producto agregado',
+        showConfirmButton: false,
+        timer: 1500,
+        width: '25em',
+        toast: true,
+        timerProgressBar: true
+      })
+      mostrarProductos()
+    }
+  }
+// función para mostrar el carrito una vez se haya agregado el producto 
+function mostrarProductos(){
+    if (carrito.length > 0) {
+     mostarTablas.style.display="block"
+     let tablaBucle= ''
+       for(let i = 0; i < carrito.length; i++){
+          tablaBucle += `<tr>
+                   <td>${carrito[i].nombre}</td>
+                   <td>${carrito[i].categoria}</td>
+                   <td>$${carrito[i].precio}</td>
+                   <td>${carrito[i].cantidad}</td>
+                   <td>$${(carrito[i].precio * carrito[i].cantidad).toFixed(2)}</td>
+                   <td align="center"><a class="eliminar" onclick="eliminarProductos(${i})"> Eliminar </a></td>
+                   </tr> `
+       }
+         tablaParaMostrar.innerHTML = tablaBucle
+         sumadoraProductos()
+     }
+ }
+ // función para eliminar el producto del carrito apartir de su indice
+ function eliminarProductos(i) {
+    if (i >= 0 && i < carrito.length) {
+        carrito.splice(i, 1)
+        localStorage.setItem('carrito', JSON.stringify(carrito))
+        Swal.fire({
+          position: 'top-end',
+          icon: 'error',
+          title: 'Producto eliminado',
+          showConfirmButton: false,
+          timer: 1500,
+          width: '25em',
+          toast: true,
+          timerProgressBar: true
+        })
+        mostrarProductos()
+    }
+  }
+//función para sumar los productos seleccionados para el carrito y en caso de que se cumpla la condición restarle el 10%
+function sumadoraProductos(){
+  let total = 0
+  const pre = carrito.map( preXcan => preXcan.cantidad * preXcan.precio)
   pre.forEach(valordeproducto => { total = total + valordeproducto })
   let valorFinal = total
-  if (valorFinal >= 1000){
-      valorFinal = valorFinal*0.9   
+  if (valorFinal >= 500){
+      valorFinal = valorFinal*0.9
          let mostrarValor = `<tr>
-             <td colspan="5" align="right"> Total</td>
-             <td colspan="1">${total}</td>
+             <td colspan="4" align="right"> Total</td>
+             <td colspan="1">$${total.toFixed(2)}</td>
+             <td colspan="1"></td>
              </tr>
              <tr>
-             <td colspan="5" align="right"> Total con 10% descuento </td>
-             <td colspan="1">${valorFinal}</td>
+             <td colspan="4" align="right"> Total con 10% descuento </td>
+             <td colspan="1">$${valorFinal.toFixed(2)}</td>
+             <td colspan="1"><a class="comprar btn btn-primary" onclick="confirmarCompra(${valorFinal.toFixed(2)})"> Comprar </a></td>
              </tr> `
              infoDelTotal.innerHTML=mostrarValor
   } else {
          let mostrarValor = `<tr>
-         <td colspan="5" align="right"> Total</td>
-         <td colspan="1">${total}</td>
+         <td colspan="4" align="right"> Total</td>
+         <td colspan="1">$${total.toFixed(2)}</td>
+         <td colspan="1"><a class="comprar btn btn-primary" onclick="confirmarCompra(${total.toFixed(2)})"> Comprar </a></td>
          </tr>
          <tr> `
-         infoDelTotal.innerHTML=mostrarValor  
+         infoDelTotal.innerHTML=mostrarValor
   }
-     console.log(total)
-     console.log(valorFinal)
+     return valorFinal
  }
- //función para eliminar los datos del array y del localStorage
- function eliminarProductos(i) {
-  if (i >= 0 && i < carrito.length) {
-      carrito.splice(i, 1)
-      localStorage.setItem('carrito', JSON.stringify(carrito))
-      mostrarProductos()
-  }
-}
+ // función que muestra alerta al confirmar la compra 
+ function confirmarCompra(valorFinal){
+  Swal.fire({
+    title: 'Confirmar compra',
+    text: "Estás seguro que quieres confirmar tu compra?",
+    icon: 'question',
+    backdrop: 'rgba(0, 0, 0, 0.8)',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    cancelButtonText: 'No, ir atrás',
+    confirmButtonText: 'Confirmar'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      carrito.splice(0, carrito.length);
+      localStorage.removeItem('carrito');
+      mostrarProductos();
+      Swal.fire(
+        'Felicidades',
+        'Confirmaste tu compra con un total de $'+ valorFinal.toFixed(2),
+        'success'
+      ).then(() => {
+        location.reload();
+      });
+    }
+  })
+ }
+mostrarDiv();
 
